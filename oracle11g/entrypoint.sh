12e0ba1dc840d00493c16c8b2b5047fbb8312b6e
Danny Bastos <danny.bastos.br@gmail.com>
2019-04-06 05:42:11 -0300

    [wip] initial commit

---
+++ oracle11g/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+export LC_ALL="en_US.UTF-8"
+echo ". /u01/app/oracle/product/11.2.0/xe/bin/oracle_env.sh" >> /etc/profile
+source /etc/profile
+
+sed -i -E "s/HOST = [^)]+/HOST = $HOSTNAME/g" $ORACLE_HOME/network/admin/listener.ora
+sed -i -E "s/HOST = [^)]+/HOST = $HOSTNAME/g" $ORACLE_HOME/network/admin/tnsnames.ora
+
+/etc/init.d/oracle-xe start
+
+tailf /u01/app/oracle/diag/rdbms/xe/XE/trace/alert_XE.log
